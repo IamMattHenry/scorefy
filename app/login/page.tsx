e48d337436b37f94a050cedcfb525d5b354c3352
IamMattHenry <matthenry1005200@gmail.com>
2025-05-26 18:14:13 +0800

    fix deployment conflict

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold font-anton">Login</h1>
+      <p className="text-xl font-blinker text-white/90">
+        This is the login page.
+      </p>
+    </div>
+  );
+}
